14b4a3a7645af360f31a16517d9b1ced3dbae9c9
Eme-Hache <luisalfonsodsg@outlook.com>
2022-04-20 15:59:40 -0500

    API fetch and crypto list

--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -1,18 +1,20 @@
 import useSelectMonedas from '../hooks/useSelectMonedas'
+import { useEffect, useState } from 'react'
 import { monedas } from '../data/monedas'
 import styled from '@emotion/styled'
 
 const InputSubmit = styled.input`
     background-color: #9497FF;
-    border: none;
     width: 100%;
     padding: 10px;
     color: #FFF;
+    border: none;
     font-weight: 700;
     text-transform: uppercase;
     font-size: 20px;
     border-radius: 5px;
     transition: background-color .3s ease;
+    margin-top: 30px;
 
     &:hover {
         cursor: pointer;
@@ -21,12 +23,34 @@ const InputSubmit = styled.input`
 `
 
 const Formulario = () => {
-    const [SelectMonedas] = useSelectMonedas('Elige tu moneda', monedas)
+    const [criptos, setCriptos] = useState([])
+    const [moneda, SelectMonedas] = useSelectMonedas('Elige tu moneda', monedas)
+    const [criptomoneda, SelectCriptomoneda] = useSelectMonedas('Elige tu criptomoneda', criptos)
+
+    useEffect(() => {
+        const consultarAPI = async () => {
+            const url = 'https://min-api.cryptocompare.com/data/top/mktcapfull?limit=20&tsym=USD'
+
+            let resultado = await fetch(url)
+            resultado = await resultado.json()
+
+            const arrayCriptos = resultado.Data.map(({ CoinInfo }) => (
+                {
+                    id: CoinInfo.Name,
+                    nombre: CoinInfo.FullName
+                }
+            ))
+
+            setCriptos(arrayCriptos)
+        }
+
+        consultarAPI()
+    }, [])
 
     return (
         <form>
             <SelectMonedas />
-
+            <SelectCriptomoneda />
             <InputSubmit type="submit" value="Cotizar" />
         </form>
     )
--- src/hooks/useSelectMonedas.jsx
+++ src/hooks/useSelectMonedas.jsx
@@ -1,14 +1,31 @@
+import { useState } from 'react'
 import styled from '@emotion/styled'
 
 const Label = styled.label`
    color: #FFF; 
+   display: block;
+   font-family: 'Lato', sans-serif;
+   font-size: 24px;
+   font-weight: 700;
+   margin: 15px 0;
+`
+const Select = styled.select`
+    width: 100%;
+    font-size: 18px;
+    padding: 14px;
+    border-radius: 10px;
 `
 
 const useSelectMonedas = (label, opciones) => {
+    const [state, setState] = useState('')
+
     const SelectMonedas = () => (
         <>
             <Label>{label}</Label>
-            <select>
+            <Select
+                value={state}
+                onChange={evt => setState(evt.target.value)}
+            >
                 <option value=''>Seleccione</option>
 
                 {opciones.map(opcion => (
@@ -19,11 +36,11 @@ const useSelectMonedas = (label, opciones) => {
                         {opcion.nombre}
                     </option>
                 ))}
-            </select>
+            </Select>
         </>
     )
 
-    return [SelectMonedas]
+    return [state, SelectMonedas]
 }
 
 export default useSelectMonedas
